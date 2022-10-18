52ff7dc5688746c7b8c237d0d5c42c4ca86a969f
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-18 15:31:01 +0000

    add settings view with settingslabel view

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -19,9 +19,30 @@ struct SettingsView: View {
         NavigationView {
             ScrollView(.vertical, showsIndicators: false) {
                 VStack(spacing: 20) {
-                    Text("HELLO")
+                    
+                    //MARK: Section 1
+                    
+                    GroupBox(label:
+                                SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
+                    ) {
+                        Divider().padding(.vertical, 4)
+                        HStack(alignment: .center, spacing: 10){
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .cornerRadius(9)
+                            Text("Most fruits are naturally low in fat, sodium, and calories. None have cholesterol. Fruits are sources of many essential nutrients, including potassium, dietary fiber, vitamins, and much more")
+                                .font(.footnote)
+                        }
+                    }
+                    //MARK: Section 2
+
+                    //MARK: Section 3
+
                 }//: VStack
                 .navigationTitle("Settings")
+                .padding()
                 .navigationBarTitleDisplayMode(.large)
                 .navigationBarItems(
                     trailing: Button(action: {
