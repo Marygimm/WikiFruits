0f98465b1310de77ea70bb7a6074c2743335a97b
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-18 11:06:35 +0000

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Mary Moreira on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
