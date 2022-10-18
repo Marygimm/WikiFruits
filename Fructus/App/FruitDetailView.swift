3e6ac187fa54c3aec2bf5743e0cfaf418d0fc842
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-18 15:05:17 +0000

    add new view to fruit detail

--- Fructus/App/FruitDetailView.swift
+++ Fructus/App/FruitDetailView.swift
@@ -36,6 +36,8 @@ struct FruitDetailView: View {
                         
                         //NUTRIENTS
                         
+                        FruitNutrientsView(fruit: fruit)
+                        
                         //SUBHEADLINE
                         
                         Text("Learn more about \(fruit.title)".uppercased())
